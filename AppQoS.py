b7250491934c4cea40dfad82178b91c7ccf408f1
Bruno Martins <sgtbrunoalmeida@gmail.com>
2019-05-10 19:11:35 -0300

    10/05/2019 19:11

--- AppQoS.py
+++ AppQoS.py
@@ -6,26 +6,18 @@ from ryu.ofproto import ofproto_v1_3
 #Pacotes
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
 from ryu.lib.packet import arp
 from ryu.lib.packet import ipv4
 from ryu.lib.packet import icmp
 from ryu.lib.packet import tcp
 from ryu.lib.packet import udp
 #Extras
-from ryu.lib.dpid import dpid_to_str
 from ryu.ofproto.ofproto_v1_2 import OFPG_ANY
-from ryu.lib.mac import haddr_to_bin
-#Topologia
-from ryu.topology import event, switches
-from ryu.topology.api import get_switch, get_link
-#import networkx as nx
+from ryu.ofproto import ether
 #Sistema
 import os
 import time
-import requests
 import pickle
-from classe import Classe
 
 import Tabela_IP_Porta as tb
 
@@ -40,6 +32,8 @@ MAC_SERVIDOR = tb.mac_servidor
 BROADCAST_MAC = 'ff:ff:ff:ff:ff:ff'
 
 ZERO_MAC = '00:00:00:00:00:00'
+
+PORTA_SERVIDOR = 7 #ARRUMAR DEPOIS
 
 path_home = os.getenv("HOME") #Captura o caminho da pasta HOME
 
@@ -73,17 +67,19 @@ class AppQoS(app_manager.RyuApp):
 
         #Regra de QoS de acordo com a tabela passada
         time.sleep(1)
+        print('')
         for regra in TABELA_IP_PORTAS:
             #os.system('ovs-ofctl add-flow s1 priority=40000,dl_type=0x0800,nw_dst=' + regra[0] + ',nw_proto=17,tp_dst=' + regra[1] + ',actions=enqueue:' + regra[2] + ':' + self.filaQoS(regra[3]))
             #os.system('ovs-ofctl add-flow s1 priority=40000,dl_type=0x0800,nw_dst=' + regra[0] + ',nw_proto=6,tp_dst=' + regra[1] + ',actions=enqueue:' + regra[2] + ':' + self.filaQoS(regra[3]))
             match1 = parser.OFPMatch(eth_type=0x0800,ipv4_dst=regra[0],ip_proto=6,tcp_dst=int(regra[1]))
-            actions1 = [parser.OFPActionSetQueue(int(self.filaQoS(regra[3]))), parser.OFPActionOutput(port=7)]
+            actions1 = [parser.OFPActionSetQueue(int(self.filaQoS(regra[3])[0])), parser.OFPActionOutput(port=PORTA_SERVIDOR)]
             self.add_flow(datapath, 40000, match1, actions1, 0, 28)
+            print('QoS: '+ self.filaQoS(regra[3])[1] + ' aplica na porta: ' + regra[1] + ' com destino ao servidor!')
 
         #Tudo que for TCP envia para o controlador
         match2 = parser.OFPMatch(eth_type=0x0800, ip_proto=6)
         self.add_flow(datapath, 1000, match2, actions, 0, 29)
-        print('Eventos de Entradas Prontos!!!')
+        print('\nEventos de Entradas Prontos!!!\n')
 
     '''
     FIM Eventos de Inicio de Topologia ou chegada de Switch
@@ -94,7 +90,7 @@ class AppQoS(app_manager.RyuApp):
        '''
 
     def add_flow(self, datapath, priority, match, actions, idle_timeout=0, numero=0):
-        print('Numero DEBUG: '+ str(numero))
+        #print('Numero DEBUG: '+ str(numero))
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
@@ -128,11 +124,13 @@ class AppQoS(app_manager.RyuApp):
             classlist = pickle.load(filec)
             filec.close()
         fila_saida = '0'  # Fila a ser aplicada
+        nome_fila = ''
         for c in classlist:
             if c.nome == nome:
                 fila_saida = c.id
+                nome_fila = c.nome
         #print('Fila saida: ' + str(fila_saida))
-        return str(fila_saida)
+        return [str(fila_saida),nome_fila]
 
     '''
     FIM Fila requerida para QoS
@@ -176,10 +174,9 @@ class AppQoS(app_manager.RyuApp):
             out_port = self.mac_to_port[dpid][dst]
         else:
             out_port = ofproto.OFPP_FLOOD
-        # lista de acoes de construcao de regra.
+        #lista de acoes de construcao de regra.
         actions = [parser.OFPActionOutput(out_port)]
 
-        #Aqui 1
         # Tipos de pacotes
         pkt_ethernet = pkt.get_protocol(ethernet.ethernet)
         pkt_arp = pkt.get_protocol(arp.arp)
@@ -192,72 +189,139 @@ class AppQoS(app_manager.RyuApp):
         if pkt_arp: #arrumar a funcao
             # aprende o ip e o mac
             self.ip_to_mac[dpid][pkt_arp.src_ip] = [pkt_arp.src_mac, in_port] #Ex: {1: {'10.0.0.7': ['00:00:00:00:00:07', 7], '10.0.0.1': ['00:00:00:00:00:01', 1]}}
-            #print(self.ip_to_mac)
+            #print(self.ip_to_mac) #DEBUG
             if pkt_arp.opcode == 1: #Arp Request
-                print('Arp Request de ' + pkt_arp.src_ip + ' para ' + pkt_arp.dst_ip)
-                #self.arp_reply(datapath, in_port, eth_pkt, pkt_arp, dpid)  # Responde o ARP
-
-                if pkt_arp.dst_ip is self.ip_to_mac[dpid]:
-                    self.arp_reply(datapath, in_port, eth_pkt, pkt_arp, dpid)  # Responde o ARP
-                else: #Inunda a rede
-                    out = parser.OFPPacketOut(datapath=datapath,
-                                              buffer_id=ofproto.OFP_NO_BUFFER,
-                                              in_port=in_port, actions=actions,
-                                              data=msg.data)
-                    datapath.send_msg(out)
-
+                #print('\nArp Request de ' + pkt_arp.src_ip + ' para ' + pkt_arp.dst_ip + '\n') #DEBUG
+                if dpid in self.ip_to_mac: #Verifica se ja existe informacao do switch em questao
+                    if pkt_arp.dst_ip in self.ip_to_mac[dpid]: #Se conhece o mac da maquina responde. Se nao, inunda a rede para descobrir
+                        self.arp_reply(datapath, in_port, eth_pkt, pkt_arp, dpid)  # Responde o ARP
+                        #print('\nResponde o arp \n') #DEBUG
+                        return
+                    else: #Inunda a rede
+                        #print('\nExiste o dpid, mas nao possui o mac destino ARP!\n') #DEBUG
+                        src_mac = src
+                        src_ip = pkt_arp.src_ip
+                        dst_ip = pkt_arp.dst_ip
+                        self.envia_pkt_broadcasting(datapath, parser, ofproto, in_port, src_mac, src_ip, dst_ip, 'arp_1')
+                else: #Nao existe, entao vamos coletar
+                    #print('\nNao existe o dpid\n') #DEBUG
+                    src_mac = src
+                    src_ip = pkt_arp.src_ip
+                    dst_ip = pkt_arp.dst_ip
+                    self.envia_pkt_broadcasting(datapath, parser, ofproto, in_port, src_mac, src_ip, dst_ip, 'arp_2')
 
             elif pkt_arp.opcode == 2: #Arp Reply
-                print('Arp Reply de ' + pkt_arp.src_ip + ' para ' + pkt_arp.dst_ip)
-                self.arp_reply(datapath, in_port, eth_pkt, pkt_arp, dpid) #Responde o ARP
+                pass
+                #print('\nArp Reply de ' + pkt_arp.src_ip + ' para ' + pkt_arp.dst_ip + '\n') #DEBUG
             else:
                 pass
 
         #if pkt_tcp and pkt_ipv4.src == IP_SERVIDOR:
         if pkt_tcp:
+            self.ip_to_mac[dpid][pkt_ipv4.src] = [src,
+                                                    in_port]  # Ex: {1: {'10.0.0.7': ['00:00:00:00:00:07', 7], '10.0.0.1': ['00:00:00:00:00:01', 1]}}
             vet_portas_servidor = []
             for linha in TABELA_IP_PORTAS:
                 vet_portas_servidor.append(int(linha[1])) #Pega a posicao das portas da tabela
             if pkt_tcp.src_port in vet_portas_servidor:
-                print('Pacote vindo do servidor')
+                #print('\nPacote vindo do servidor\n') #DEBUG
                 ofp = datapath.ofproto
-                #match = parser.OFPMatch(eth_type='0x0800',in_port=in_port, ipv4_dst=pkt_ipv4.dst, tcp_dst=pkt_tcp.dst_port,)
                 match = parser.OFPMatch(in_port=in_port,eth_type=0x0800,ipv4_src=pkt_ipv4.src, ipv4_dst=pkt_ipv4.dst,
                                         ip_proto=6,tcp_src=pkt_tcp.src_port,tcp_dst=pkt_tcp.dst_port)
-
-                for regra in TABELA_IP_PORTAS:
-                    if int(regra[1]) == pkt_tcp.src_port:
-                        actions = [parser.OFPActionSetQueue(int(self.filaQoS(regra[3]))), parser.OFPActionOutput(port=self.mac_to_port[dpid][dst])]
+                #Caso nao conheca os mac, entao procuro
+                if dpid in self.mac_to_port:
+                    #print('\nExiste o dpid\n') #DEBUG
+                    if pkt_ipv4.dst in self.ip_to_mac[dpid]:
+                        for regra in TABELA_IP_PORTAS:
+                            if int(regra[1]) == pkt_tcp.src_port:
+                                qos = self.filaQoS(regra[3])
+                                actions = [parser.OFPActionSetQueue(int(qos[0])), parser.OFPActionOutput(
+                                    port=self.mac_to_port[dpid][dst])]
+                                self.add_flow(datapath, 1005, match, actions, 60, 27)
+                                print('\nAdicionado a regra TCP com QoS')
+                                print('Tudo que for de '+ str(pkt_ipv4.src)+' para '+str(pkt_ipv4.dst)+' na porta '+str(
+                                    pkt_tcp.dst_port)+' enqueue '+qos[1])
+                                print('')
+                                break
+                        return
+                    else:
+                        #print('\nMAC: '+str(dst)+' nao encontrado na lista...\nProcurando o MAC...\n') #DEBUG
+                        src_mac = src
+                        src_ip = pkt_ipv4.src
+                        dst_ip = pkt_ipv4.dst
+                        self.envia_pkt_broadcasting(datapath, parser, ofproto, in_port, src_mac, src_ip, dst_ip,
+                                                    'tcp_1')
+                else:
+                    #print('\nNao existe o dpid TCP\n') #DEBUG
+                    src_mac = src
+                    src_ip = pkt_ipv4.src
+                    dst_ip = pkt_ipv4.dst
+                    self.envia_pkt_broadcasting(datapath, parser, ofproto, in_port, src_mac, src_ip, dst_ip, 'tcp_2')
+                    return
+            else: #Pacote sem QoS. Cai na fila 0 (Zero)
+                if dpid in self.mac_to_port: #Existe o switch na tabela
+                    if pkt_ipv4.dst in self.ip_to_mac[dpid]:
+                        match = parser.OFPMatch(in_port=in_port, eth_type=0x0800, ipv4_src=pkt_ipv4.src, ipv4_dst=pkt_ipv4.dst,
+                                                ip_proto=6, tcp_src=pkt_tcp.src_port, tcp_dst=pkt_tcp.dst_port)
+                        actions = [parser.OFPActionOutput(port=self.mac_to_port[dpid][dst])]
                         self.add_flow(datapath, 1005, match, actions, 60, 27)
-                        break
-            else:
-                match = parser.OFPMatch(in_port=in_port, eth_type=0x0800, ipv4_src=pkt_ipv4.src, ipv4_dst=pkt_ipv4.dst,
-                                        ip_proto=6, tcp_src=pkt_tcp.src_port, tcp_dst=pkt_tcp.dst_port)
-                actions = [parser.OFPActionOutput(port=self.mac_to_port[dpid][dst])]
-                self.add_flow(datapath, 1005, match, actions, 60, 27)
-
-
-
+                        print('\nRegra comum aplicada!!\nTudo que for de ' + str(src) + ' para ' + str(
+                            dst) + ' sai na porta ' + str(out_port) + '\n')
+                    else:
+                        src_mac = src
+                        src_ip = pkt_ipv4.src
+                        dst_ip = pkt_ipv4.dst
+                        self.envia_pkt_broadcasting(datapath, parser, ofproto, in_port, src_mac, src_ip, dst_ip,
+                                                    'tcp_2')
+                else:
+                    src_mac = src
+                    src_ip = pkt_ipv4.src
+                    dst_ip = pkt_ipv4.dst
+                    self.envia_pkt_broadcasting(datapath, parser, ofproto, in_port, src_mac, src_ip, dst_ip,
+                                                'tcp_3')
 
 
         #Serve para demais regras que nao sao tcp
         # instala um fluxo no switch para evitar o packet_in na proxima vez
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_src=src, eth_dst=dst)
-            self.add_flow(datapath, 1, match, actions, 10)
-
-        #Constroi o pacote de saida e envia
-
-        #out = parser.OFPPacketOut(datapath=datapath,
-        #                          buffer_id=ofproto.OFP_NO_BUFFER,
-        #                          in_port=in_port, actions=actions,
-        #                          data=msg.data)
-        #datapath.send_msg(out)
-        #print('To aqui!! ' +str(in_port) )
+        if pkt_udp or pkt_icmp:
+            if out_port != ofproto.OFPP_FLOOD:
+                match = parser.OFPMatch(in_port=in_port, eth_src=src, eth_dst=dst)
+                self.add_flow(datapath, 1, match, actions, 30)
+                print('\nRegra comum aplicada!!\nTudo que for de '+str(src)+' para '+str(dst)+' sai na porta  ' +str(
+                    out_port)+'\n')
 
     '''
     FIM Area do Packet_In
     '''
+
+    '''
+    Pacote Broadcasting
+    '''
+    def envia_pkt_broadcasting(self, datapath, parser, ofproto, in_port, src_mac, src_ip, dst_ip, debug_proto=''):
+        #print(debug_proto) #DEBUG
+        e = ethernet.ethernet(dst = BROADCAST_MAC,
+                              src = src_mac,
+                              ethertype = ether.ETH_TYPE_ARP)
+        a = arp.arp(hwtype=1, proto=0x800, hlen=6, plen=4, opcode=1,
+                    src_mac= src_mac,src_ip= src_ip,
+                    dst_mac=ZERO_MAC,dst_ip=dst_ip)
+        p = packet.Packet()
+        p.add_protocol(e)
+        p.add_protocol(a)
+        p.serialize()
+
+        actions4 = [parser.OFPActionOutput(ofproto.OFPP_FLOOD, 0)]
+        out = parser.OFPPacketOut(datapath=datapath,
+                                  buffer_id=ofproto.OFP_NO_BUFFER,
+                                  in_port=in_port, actions=actions4,
+                                  data=p.data)
+        datapath.send_msg(out)
+        #print('\nInunda a rede \n') #DEBUG
+    '''
+    FIM Pacote Broadcasting
+    '''
+
+
 
     '''
     Arp Reply
@@ -344,8 +408,3 @@ Tudo o que eu quiser iniciar, basta colocar aqui!!
 '''
 # Require
 #app_manager.require_app('ryu.app.ofctl_rest')
-#app_manager.require_app('ryu.app.simple_switch_13_mod')
-#app_manager.require_app('ryu.app.simple_switch_13')
-#app_manager.require_app('ryu.app.rest_conf_switch')
-#app_manager.require_app('ryu.app.rest_topology')
-# app_manager.require_app('ryu.app.rest_qos_mod')
